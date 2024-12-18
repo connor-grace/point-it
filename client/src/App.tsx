fc32b56fec5ec227471394d8539c7423e5fb88ac
connor-grace <connor.grace88@gmail.com>
2024-12-17 23:43:00 -0500

    http server and websocket on same port

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-    const [data, setData] = React.useState(null);
+export default function App() {
+    //const [data, setData] = React.useState(null);
 
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
+    //React.useEffect(() => {
+    //    fetch("/api")
+    //        .then((res) => res.json())
+    //        .then((data) => setData(data.message));
+    //}, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>{!data ? "Loading..." : data}</p>
-            </header>
+        <div>
+            <h1>Point-It</h1>
+            <h2>Where pointing is a pleasure</h2>
+
+            <h3>Points</h3>
+            <input type="radio" id="f1" name="points" value="1" />
+            <label htmlFor="f1">1</label>
+            <input type="radio" id="option2" name="points" value="2" />
+            <label htmlFor="f2">2</label>
+            <input type="radio" id="option3" name="points" value="3" />
+            <label htmlFor="f3">3</label>
+            <input type="radio" id="option5" name="points" value="5" />
+            <label htmlFor="f5">5</label>
+            <input type="radio" id="option8" name="points" value="8" />
+            <label htmlFor="f8">8</label>
+            <input type="radio" id="option13" name="points" value="13" />
+            <label htmlFor="f13">13</label>
+            <input type="radio" id="option21" name="points" value="21" />
+            <label htmlFor="f21">21</label>
         </div>
     );
 }
-
-export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,34 @@
+import { randomUUID } from "crypto";
 import express from "express";
+import http from "http";
+import { WebSocket } from "ws";
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function connection(ws) {
+  const userId = randomUUID();
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("Welcome " + userId);
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+server.listen(PORT, () => {
+  console.log("Server listening on %d", PORT);
 });
+
+// multiple users
+// user needs to tell server it has pointed
+// server needs to relay that user has pointed back to all other users to update ui
+// server needs to let clients know when all users have pointed so they can enable flipping cards
+//
